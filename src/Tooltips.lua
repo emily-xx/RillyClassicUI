a8e42d10a971f9716e983f554e6204ac949cdf89
Emma Chatley <emchatley@gmail.com>
2019-09-04 16:30:28 +0100

    This function is not very reliable

--- src/Tooltips.lua
+++ src/Tooltips.lua
@@ -39,23 +39,4 @@ CF:SetScript("OnEvent", function(self, event)
 	GameTooltip:HookScript("OnUpdate", function(tooltip)
 		GameTooltip:SetBackdropColor(0.13,0.13,0.13) -- Simpler and themed BG color
 	end)
-
-	GameTooltipStatusBar:HookScript("OnValueChanged", function(self, hp)
-		TextStatusBar_UpdateTextString(self)
-	  local unit = "mouseover"
-	  local focus = GetMouseFocus()
-	  if (focus and focus.unit) then
-	      unit = focus.unit
-	  end
-	  local r, g, b
-
-	  if (UnitIsPlayer(unit)) then
-	    r, g, b = GetClassColor(select(2,UnitClass(unit)))
-	  else
-	    r, g, b = GameTooltip_UnitColor(unit)
-	    if (g == 0.6) then g = 0.9 end
-	    if (r==1 and g==1 and b==1) then r, g, b = 0, 0.9, 0.1 end
-	  end
-	  self:SetStatusBarColor(r, g, b)
-	end)
 end)
