b08885194a4e72893358efdca4c8c8eb18cc9b85
Emma Chatley <emchatley@gmail.com>
2019-09-04 19:15:56 +0100

    Removed unused config items

--- src/Config.lua
+++ src/Config.lua
@@ -1,28 +1,10 @@
 -- Setting any value here to nil will result in Blizzard defaults, if you want those instead.
 RCConfig = {
-  actionBarOffset = 20,
-  disableAutoAddSpells = true, -- Whether or not to disable the automatic addition of spells to bars when changing talents and etc
   castbarOffset = 160,
-
-  pulltimer = true,
-
-  lootSpecDisplay = true, -- Display loot spec under the player frame
 
   damageFont = true, -- Change damage font to something cooler
 
-  -- Nameplate Settings
-  modNamePlates = true, -- Set to false to ignore all nameplate customization
-  nameplateNameFontSize = 7,
-  nameplateHideServerNames = true,
-  nameplateNameLength = 20, -- Set to nil for no abbreviation
-  nameplateFriendlyNamesClassColor = true,
-  namePlateWidth = 120,
-  namePlateScale = 1.3,
-  nameplateHideCastText = false,
-  nameplateCastFontSize = 6,
-
   portraitStyle = "3D", -- 3D, 2D, or class (for class icons)
-  objectivesTitles = "class", -- Class for class coloured quest titles, or default for default
   objectivesTextOutline = false,
   minimapZoneText = true, -- True = show zone text, False = hide
 }
